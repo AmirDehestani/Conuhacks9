ba46f3de0f4b5bb142156fecb4feacbae60b69ad
fatrifatrifat <rifatkatranci19@gmail.com>
2025-02-01 12:06:42 -0500

    React routes setup for info, createJoin and lobby

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const http = require('http');
+const path = require('path')
+const { Server } = require('socket.io');
+
+const PORT = 5000;
+
 const app = express();
-const port = 5000;
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-app.listen(port, () => {    
-  console.log(`Server is running at http://localhost:${port}`);
-});
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`);
+})
 
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+})
